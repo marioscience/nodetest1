b85598fe6b15050b7a7d6eec0485121173169a67
mmatos <marioscience@hotmail.com>
2017-01-17 11:16:11 -0700

    Added form to add new user to the database and show user list on success

--- routes/index.js
+++ routes/index.js
@@ -23,4 +23,39 @@ router.get('/userlist', function (req, res) {
   });
 });
 
+/* GET new user page. */
+router.get('/newuser', function (req, res) {
+    res.render('newuser', { title: 'Add New User'});
+});
+
+/* POST to Add User Service */
+router.post('/adduser', function (req, res) {
+
+  /* Set internal DB variable */
+  var db = req.db;
+
+  // Get our form values. These rely on the name attributes on the inputs
+  var userName = req.body.username;
+  var userEmail = req.body.useremail;
+
+  // Set our collection
+  var collection = db.get('usercollection');
+
+  // Submit to database
+    collection.insert({
+        "username": userName,
+        "email": userEmail
+    }, function (err, doc) {
+            if (err) {
+                // It failed, return error
+                res.send("Could not add user");
+            }
+            else {
+                //Send to success page
+                res.redirect("userlist");
+            }
+        }
+    );
+});
+
 module.exports = router;
